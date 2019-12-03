2c72490a0fb6210bddfea4c9a18f3384d2eecb2b
matiya <matan.atiya@changehealthcare.com>
2019-12-03 14:05:24 +0200

    working on header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,26 @@
-import React, { useState, useEffect} from 'react';
-import './Header.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+//developed
+import "./Header.css";
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
+  const [error, setError] = useState(false);
 
- const [error, setError] = useState(false);
-
- useEffect(() => {});
+  useEffect(() => {});
 
   return (
-    <header className='Header'>
-      <h1>{ title }</h1>
+    <header className="Header">
+      <h1>{title}</h1>
+      <div className="links">
+        <Link to="/" className="link">Dashboard</Link>
+        <Link to="/create" className="link">Create Expense</Link>
+        <Link to="/edit" className="link">Edit Expense</Link>
+        <Link to="/help" className="link">Help</Link>
+      </div>
     </header>
-   );
-}
-
-Header.defaultProps = {
-
+  );
 };
+
+Header.defaultProps = {};
 
 export default Header;
