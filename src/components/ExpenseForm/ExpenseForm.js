b9960fc289b82e4447e71674aa5db6487a7b2e59
matiya <matan.atiya@changehealthcare.com>
2020-01-28 10:01:24 +0200

    working on expense form

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = props => {
-  const title = "ExpenseForm component work";
+  // Declare a new state variables
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [note, setNote] = useState("");
+
+  const onDescriptionChange = e => {
+    setDescription(e.target.value);
+  };
+
+  const onNoteChange = e => {
+    setNote(e.target.value);
+  };
+
+  const onAmountChange = e => {
+    const amount = e.target.value;
+    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+      setAmount(amount);
+    }
+  };
 
   return (
     <div className="ExpenseForm">
       <form>
-        <input type="text" placeholder="Description" autoFocus />
-        <input type="number" placeholder="Amount" />
-        <textarea placeholder="Add a note for your expense (optional)" />
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={onDescriptionChange}
+          autoFocus
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          value={amount}
+          onChange={onAmountChange}
+        />
+        <textarea
+          placeholder="Add a note for your expense (optional)"
+          value={note}
+          onChange={onNoteChange}
+        />
+        <button>Add Expense</button>
       </form>
     </div>
   );
