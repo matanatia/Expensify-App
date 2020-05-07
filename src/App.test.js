68cec29a0157b7914213d887a2667a06d6a7e308
Matan Atiya <matan.atiya302@gmail.com>
2020-05-07 12:57:03 +0300

    add notes about testing framework jest

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 
+/**
+ * for test file to work properly add 
+ 
+babel.config.js at the root of the project with:
+
+ module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current',
+                },
+            },
+        ],
+    ],
+};
+ 
+
+* !! this test file work with the test framework jest - note that jest installed automaticly in your node_modules when using creat-react-app to create the app  first time)
+*/
+
 it('renders without crashing', () => {
   // const div = document.createElement('div');
   // ReactDOM.render(<App />, div);
